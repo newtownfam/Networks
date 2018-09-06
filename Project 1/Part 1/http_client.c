6a8d8e35b4433407ef0f478ed08e81dd53d61c3b
Peter Christakos <newtownfam@gmail.com>
2018-09-06 15:41:11 -0400

    cleaned up some stuff

--- Project 1/Part 1/http_client.c
+++ Project 1/Part 1/http_client.c
@@ -38,7 +38,7 @@ void *get_address(struct sockaddr *sa) {
 
 int main(int argc, char *argv[]) {
 	struct timeval tv;
-	time_t startTime, endTime, totalTIme;
+	time_t startTime, endTime, totalTime;
 	int RTT_Flag = 0; // 0 is no, 1 is yes
 	int wait_counter = 0; // for looping through buffer and seeing EOF
 	int sockfd, numbytes;
@@ -47,7 +47,7 @@ int main(int argc, char *argv[]) {
 	int status; // error code variable
 	char s[INET6_ADDRSTRLEN]; // char of IPv6 address
 	const char * node; // values to assign to inputs
-	const char * service; // value to assign to input
+	const char * port; // value to assign to input
 	char message[100];
 	int len;
 
@@ -56,14 +56,14 @@ int main(int argc, char *argv[]) {
 		if (strcmp(argv[1], "-p") == 0) {
 			RTT_Flag = 1;
 			node = argv[2];
-			service = argv[3];
+			port = argv[3];
 			printf("\n");
 			printf("RTT Activated.\n");
 			// print RTT
 		} else { fprintf(stderr, "ERROR: Option not known\n"); exit(1); }
 	} else if (argc == 3) {
 		node = argv[1];
-		service = argv[2];
+		port = argv[2];
 	} else {
 		fprintf(stderr, "ERROR: Invalid amount of arguments\n"); exit(1);
 	}
@@ -81,16 +81,14 @@ int main(int argc, char *argv[]) {
 
 	printf("\n");
 	printf("Node: %s\n", node);
-	printf("Service: %s\n", service);
+	printf("Port: %s\n", port);
 	printf("\n");
 
-	if ((status = getaddrinfo(node, service, &info, &servinfo)) != 0) {
+	if ((status = getaddrinfo(node, port, &info, &servinfo)) != 0) {
 		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(status));
 		return 1;
 	}
-
-	printf("Got Past getaddrinfo\n");
-
+	
 	// servinfo now points to a linked list of 1 or more struct addrinfos
 
 	// record time of day for RTT purposes
@@ -100,7 +98,7 @@ int main(int argc, char *argv[]) {
 	// loop through all the results and connect to the first we can
 
 	for(p = servinfo; p != NULL; p = p->ai_next) {
-		printf("in the loop\n");
+
 		// socket function creates endpoint for communication. Returns
 		// non-negative integer for success
 		if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
@@ -109,28 +107,25 @@ int main(int argc, char *argv[]) {
 		}
 
 		if (connect(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
-			printf("Hello\n");
             close(sockfd);
             perror("Client: Connection Error");
-            continue;
+            break;
         }
 
-		printf("connected\n");
+		printf("Connect function worked!\n\n");
 
 		// record time of day for RTT purposes
 		gettimeofday(&tv, NULL);
 		endTime = tv.tv_usec;
-		totalTIme = endTime - startTime;
+		totalTime = endTime - startTime;
 
 		if (RTT_Flag == 1) {
-			printf("Connected with Round Trip Time: %ld micro sec\n", totalTIme);
+			printf("Connected with Round Trip Time: %ld micro sec\n", totalTime);
 			printf("\n");
 		}
 
 		break;
 	}
-
-	printf("got past for loop\n");
 
 	// throw null error for p if connection failed
 	if (p == NULL) {
@@ -138,7 +133,6 @@ int main(int argc, char *argv[]) {
 		return 2;
 	}
 
-	printf("client didnt fail to connect\n");
 
 	// convert IPv4 or IPv6 from binary to text
 	inet_ntop(p->ai_family, get_address((struct sockaddr *)p->ai_addr), s, sizeof s);
@@ -171,7 +165,7 @@ int main(int argc, char *argv[]) {
     }
 
  
-	printf("\nClient: Complete\n\n");
+	printf("\nClient: Complete!\n\n");
 	close(sockfd); // close socket connection
 
 	return 0;
